a2d0c1c0ba7cb2ce461c8364e6261568d9717f40
m_vol <supermishka3@gmail.com>
2022-08-17 20:40:42 +0300

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Домашнее задание 1.4 Циклы");
+        //Задание 1
+        int i = 1;
+        while (i <= 10){
+            System.out.println(i++);
+        };
+        int i2 = 10;
+
+        };
+    }

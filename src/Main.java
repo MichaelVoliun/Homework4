fc5fc9bd40c5194d9a903c035d7c91585a5b20bb
m_vol <supermishka3@gmail.com>
2022-08-21 15:56:08 +0300

    Задание 2 выполнено

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,23 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Домашнее задание 1.4 Циклы");
         //Задание 1
+        System.out.println("Задание 1");
         int i = 1;
         while (i <= 10){
             System.out.print(i++ + " ");
-        };
+        }
         System.out.println();
-        for (int p = 10; p >= 0; p = p-1){
-            System.out.print(p-- +" ");
-        };
+        for (int p = 10; p >= 1; p = p-1){
+            System.out.print(p +" ");
+        }
+        System.out.println();
         //Задание 2
+        System.out.println("Задание 2");
         int f = 5;
+
+        for (int everyFriday = f; everyFriday <= 31; everyFriday +=7) {
+            System.out.println("Сегодня пятница " + everyFriday + "-е число. Необходимо подготовить отчет");
+        }
 
     }
 }
